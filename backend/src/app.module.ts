e12561c020c1e27f0be4bb34b29bf4e017088294
the1020 <yenn2322@gmail.com>
2023-05-23 17:50:14 +0900

    Feat: mysql 연동

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dbProvider } from './db/db.provider';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, dbProvider],
 })
 export class AppModule {}
