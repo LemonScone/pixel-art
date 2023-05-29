0fa3d625426306977231bb2c3c13dbccc916f1e5
padosum <makalu171@gmail.com>
2023-05-29 20:32:30 +0900

    Chore: cookie-parser 설치 및 적용

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { setupSwagger } from './utils/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/httpExceptionFilter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
