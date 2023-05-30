ae88c7cdce24135a91697d44f103ee01b692c92a
yeeunkim <yenn2322@gmail.com>
2023-05-30 16:57:49 +0900

    [Feature] projects 관련 API 생성 (#17)
    * Feat: projects 관련 API 생성
    
    * Fix: #17 리뷰 반영 - insertId 처리, transaction 처리, frame 조회
    
    * Feat: #17 리뷰 반영 - 사용자별 projects 조회 API 생성
    
    * Feat: #17 리뷰 반영 - 주석 제거

---
+++ backend/src/projects/project.model.ts
@@ -0,0 +1,16 @@
+import { Frame } from './frame.model';
+
+export interface Project {
+  id: number;
+  userId: string;
+  animate: boolean;
+  cellSize: number;
+  gridColumns: number;
+  gridRows: number;
+  pallete: string;
+  title: string;
+  description: string;
+  isPublished: boolean;
+
+  frames: Frame[];
+}
