e4b994427bd696b083ecd56a0bd93ef013d19441
padosum <makalu171@gmail.com>
2023-05-30 09:32:41 +0900

    Feat: HTTP 헤더 추가를 위해 helmet middleware 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { setupSwagger } from './utils/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './utils/httpExceptionFilter';
+import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.use(cookieParser());
 
