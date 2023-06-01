5c81735a76637a0d607fd5d59db8921f1a1a752f
Yeonjeong Choi <makalu171@gmail.com>
2023-06-01 16:37:13 +0900

    [Enhance] 만료된 refresh token을 database에서 삭제 처리 (#26)

--- backend/src/auth/strategies/jwt-refresh.strategy.ts
+++ backend/src/auth/strategies/jwt-refresh.strategy.ts
@@ -3,13 +3,17 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from 'src/users/users.service';
 import { Request } from 'express';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh-token',
 ) {
-  constructor(private readonly usersService: UsersService) {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request) => {
@@ -18,16 +22,24 @@ export class JwtRefreshStrategy extends PassportStrategy(
       ]),
       secretOrKey: process.env.JWT_REFRESH_SECRET,
       passReqToCallback: true,
+      ignoreExpiration: true,
     });
   }
 
   async validate(req: Request, payload) {
     const refreshToken = req.cookies['refreshToken'];
+
     const refreshTokenId = await this.usersService.getRefreshTokenId(
       payload.userId,
       refreshToken,
     );
+
     if (!refreshTokenId) {
+      throw new UnauthorizedException();
+    }
+
+    if (payload.exp < Date.now() / 1000) {
+      await this.authService.logout(refreshTokenId);
       throw new UnauthorizedException();
     }
 
