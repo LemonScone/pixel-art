3e54fa552e993b01cae5b28db65b9a7f00ad8cd3
padosum <makalu171@gmail.com>
2023-06-07 21:38:31 +0900

    Feat: grid 사이즈 변경 util 함수, test 추가

---
+++ frontend/src/utils/grid.ts
@@ -0,0 +1,48 @@
+export const resizeGrid = (
+  grid: string[],
+  originRows: number,
+  originColumns: number,
+  newRows: number,
+  newColumns: number
+) => {
+  let newGrid = grid;
+  const originCells = originRows * originColumns;
+
+  if (originColumns !== newColumns) {
+    // resize by columns
+    const diff = Math.abs(newColumns - originColumns);
+    const increment = newColumns - originColumns > 0;
+    if (increment) {
+      for (let i = originCells; i > 0; i -= originColumns) {
+        const pixels = Array(diff).fill("");
+        newGrid = [...newGrid.slice(0, i), ...pixels, ...newGrid.slice(i)];
+      }
+    } else {
+      for (let i = originCells - 1; i > 0; i -= originColumns) {
+        for (let j = 0; j < diff; j++) {
+          newGrid = [...newGrid.slice(0, i - j), ...newGrid.slice(i + 1 - j)];
+        }
+      }
+    }
+  }
+
+  if (originRows !== newRows) {
+    // resize by rows
+    const diff = Math.abs(newRows - originRows);
+    const increment = newRows - originRows > 0;
+    if (increment) {
+      for (let i = 0; i < newColumns; i++) {
+        const pixels = Array(diff).fill("");
+        newGrid = [...newGrid, ...pixels];
+      }
+    } else {
+      for (let i = 0; i < newColumns; i++) {
+        for (let j = 0; j < diff; j++) {
+          newGrid = newGrid.slice(0, -1);
+        }
+      }
+    }
+  }
+
+  return newGrid;
+};
