2c0440befa5f2f9a4ae32e8e08da867b1f63db14
padosum <makalu171@gmail.com>
2023-06-27 14:59:37 +0900

    Fix: title 색상 수정

--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,6 +1,6 @@
 const Gallery = () => {
   return (
-    <div className="flex items-center justify-center text-3xl text-rose-500">
+    <div className="flex items-center justify-center text-3xl text-primary-color">
       Gallery
     </div>
   );
