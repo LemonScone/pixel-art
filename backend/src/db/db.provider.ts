6e5f86552146c036e81621fc69cf6a9f903e1ac3
padosum <makalu171@gmail.com>
2023-07-21 09:29:49 +0900

    Fix: 환경변수를 ConfigService로 설정

--- backend/src/db/db.provider.ts
+++ backend/src/db/db.provider.ts
@@ -1,15 +1,17 @@
+import { ConfigService } from '@nestjs/config';
 import { createPool, Pool } from 'mysql2/promise';
-import { DB_CONNECTION } from 'src/constants';
+import { DB_CONNECTION } from '../constants';
 
 export const dbProvider = {
   provide: DB_CONNECTION,
-  useFactory: async (): Promise<Pool> => {
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService): Promise<Pool> => {
     const pool: Pool = createPool({
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: config.get<string>('DATABASE_HOST'),
+      port: parseInt(config.get<string>('DATABASE_PORT'), 10),
+      user: config.get<string>('DATABASE_USER'),
+      password: config.get<string>('DATABASE_PASSWORD'),
+      database: config.get<string>('DATABASE_NAME'),
       connectionLimit: 10,
     });
 
