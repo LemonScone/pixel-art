e0b6bbb7f4ec8f39f5c3c847ef96646f23c92b94
padosum <makalu171@gmail.com>
2023-07-22 09:20:36 +0900

    Fix: cookie-parser와 validationPipe를 test code에서 동작하도록 app.module로 이동

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { dbProvider } from './db/db.provider';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from './users/users.service';
 import { ProjectsModule } from './projects/projects.module';
 import { ArtworksModule } from './artworks/artworks.module';
 import { getEnvPath } from './envs/helper/env.helper';
 import { resolve } from 'path';
+import { AppService } from './app.service';
+import { APP_PIPE } from '@nestjs/core';
+import { CookieParserMiddleware } from '@nest-middlewares/cookie-parser';
 
 const envFilePath = getEnvPath(`${resolve(__dirname, '../src')}/envs`);
 
@@ -27,6 +25,18 @@ const envFilePath = getEnvPath(`${resolve(__dirname, '../src')}/envs`);
     ArtworksModule,
   ],
   controllers: [AppController],
-  providers: [AppService, dbProvider, UsersService, JwtAuthGuard, JwtService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CookieParserMiddleware).forRoutes('*');
+  }
+}
