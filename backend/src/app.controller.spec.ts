37de0092aeb911106eb6e2c4840f9364068c90b1
padosum <makalu171@gmail.com>
2023-07-22 14:53:51 +0900

    Fix: 사용하지 않는  Providers 삭제

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,11 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dbProvider } from './db/db.provider';
-import { ConfigService } from '@nestjs/config';
-import { UsersService } from './users/users.service';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
-import { JwtService } from '@nestjs/jwt';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -13,14 +8,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [
-        AppService,
-        dbProvider,
-        UsersService,
-        JwtAuthGuard,
-        JwtService,
-        ConfigService,
-      ],
+      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
