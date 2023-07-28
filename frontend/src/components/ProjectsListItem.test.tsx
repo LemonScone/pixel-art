e006faf81fdbb0abe804e02583c22f75e823f888
padosum <makalu171@gmail.com>
2023-07-28 09:28:27 +0900

    Fix: project 선택시 PATCH /users/current

--- frontend/src/components/ProjectsListItem.test.tsx
+++ frontend/src/components/ProjectsListItem.test.tsx
@@ -1,9 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import ProjectsListItem from "./ProjectsListItem";
 import projectsStore from "../tests/fixtures/projectsStore";
+import { MemoryRouter } from "react-router-dom";
+import { renderWithProviders } from "../utils/test-utils";
 
 const renderComponent = () => {
-  render(<ProjectsListItem project={projectsStore.data[0]} />);
+  renderWithProviders(
+    <MemoryRouter>
+      <ProjectsListItem project={projectsStore.data[0]} />
+    </MemoryRouter>,
+    {
+      preloadedState: {
+        projects: projectsStore,
+      },
+    }
+  );
 };
 
 describe("ProjectsListItem", () => {
