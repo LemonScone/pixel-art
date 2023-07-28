48668ce874b3d0824857feb69751419f11fcf28b
padosum <makalu171@gmail.com>
2023-07-28 15:07:38 +0900

    Style: ToastList 위치 수정

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -4,13 +4,16 @@ import ToastList from "../components/common/Toast/ToastList";
 
 const Root = () => {
   return (
-    <div className="min-h-screen bg-black">
-      <Navbar />
-      <main>
-        <Outlet />
-        <ToastList />
-      </main>
-    </div>
+    <>
+      <div className="min-h-screen bg-black">
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+
+      <ToastList />
+    </>
   );
 };
 
