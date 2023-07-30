00b5007069365ff9d7e4bd4b9eb4b1fee63003e2
padosum <makalu171@gmail.com>
2023-07-30 16:10:19 +0900

    Style: html 구조 수정 및 width 수정

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -47,28 +47,28 @@ const Editor = () => {
 
   return (
     <div className="mx-auto flex max-w-7xl flex-col justify-between md:flex-row">
-      <div className="order-2 flex flex-grow flex-col justify-between md:order-1">
-        <div className="flex flex-col items-center md:flex-row">
+      <div className="order-2 flex grow flex-col justify-between md:order-1">
+        <div className="z-10 flex flex-col items-center md:flex-row">
           <ToolConatiner />
-          <div className="flex flex-grow flex-col items-center p-10">
-            <div className="h-fit w-72 touch-none select-none sm:w-80 md:w-96 lg:w-[32rem] xl:w-[50rem]">
+          <div className="flex flex-grow flex-col items-center p-4">
+            <div className="h-fit w-72 touch-none select-none sm:w-80 md:w-96 lg:w-[30rem] xl:w-[32rem] 2xl:w-[50rem]">
               <PixelContainer />
             </div>
           </div>
         </div>
-        <div className="ml-4 mr-4 mt-4 flex flex-col pb-4">
+        <div className="-mt-4 ml-4 mr-4 flex flex-col pb-4 md:-mt-20">
           <PreviewHandler />
-          <div className="flex divide-x divide-gray-700 rounded bg-neutral-900">
+          <div className="flex divide-x divide-gray-700 rounded bg-neutral-900 ">
             <div className="p-4">
               <div className="h-24 w-36 bg-black text-white">preview</div>
             </div>
-            <div className="flex-grow">
+            <div className="flex max-w-3xl flex-nowrap overflow-x-auto">
               <FrameList />
             </div>
           </div>
         </div>
       </div>
-      <div className="order-1 mb-4 flex flex-col gap-2 divide-y divide-gray-700 rounded-b bg-neutral-900 md:order-2">
+      <div className="order-1 mb-4 flex flex-col gap-1 divide-y divide-gray-700 rounded-b bg-neutral-900 md:order-2">
         <div className="flex flex-col gap-2 p-4">
           <NewProject />
           <div className="flex grow justify-center gap-2">
