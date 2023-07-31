cd8fb97676506720fd2ab90235764c5cfe4d5bd4
padosum <makalu171@gmail.com>
2023-07-31 14:21:23 +0900

    Fix: Projects 불러오는 기능을 버튼에서 Modal로 이동

--- frontend/src/components/LoadProject.tsx
+++ frontend/src/components/LoadProject.tsx
@@ -1,50 +1,21 @@
-import { Modal } from "./common/Modal";
-
-import { useModal } from "../hooks/useModal";
-
-import ProjectsListItem from "./ProjectsListItem";
-import { useLazyFetchProjectsQuery } from "../store";
-import Skeleton from "./common/Skeleton";
+import { openModal } from "../store";
+import { useSearchParams } from "react-router-dom";
+import { useAppDispatch } from "../hooks/useRedux";
 
 const LoadProject = () => {
-  const [getProjects, { data, error, isLoading }] = useLazyFetchProjectsQuery();
-  const { open, openModal, closeModal } = useModal();
-
-  let content;
-
-  if (isLoading) {
-    content = <Skeleton className="h-48 w-48" times={3} />;
-  } else if (error) {
-    content = <div>Error loading projects</div>;
-  } else if (data) {
-    if (data.length) {
-      content = data.map((project) => {
-        return <ProjectsListItem key={project.id} project={project} />;
-      });
-    } else {
-      content = <div>No projects found.</div>;
-    }
-  }
-
+  const [params, setParams] = useSearchParams();
+  const dispatch = useAppDispatch();
   return (
     <>
       <button
         className="grow rounded bg-input-color px-2 py-1 text-sm text-gray-100 hover:bg-input-color-hover"
         onClick={() => {
-          getProjects();
-          openModal();
+          setParams({ ...params, modal: "true" });
+          dispatch(openModal("LoadProjects"));
         }}
       >
         LOAD
       </button>
-      <Modal.Frame open={open()} onClose={closeModal} size="6xl">
-        <Modal.Head>Load Projects</Modal.Head>
-        <Modal.Body>
-          <div className="flex flex-wrap items-stretch justify-center gap-8">
-            {content}
-          </div>
-        </Modal.Body>
-      </Modal.Frame>
     </>
   );
 };
