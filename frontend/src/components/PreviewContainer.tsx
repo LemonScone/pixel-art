9f1687989fcde8733d890d2e7c812ca70f22b04b
padosum <makalu171@gmail.com>
2023-07-31 14:23:33 +0900

    Feat: Preview 재생,정지,확대,새창 열기 추가

---
+++ frontend/src/components/PreviewContainer.tsx
@@ -0,0 +1,49 @@
+import { ClockIcon } from "@heroicons/react/24/outline";
+import Preview from "./Preview";
+import PreviewHandler from "./PreviewHandler";
+import { useState } from "react";
+
+const CELL_SIZE_SM = 3;
+const CELL_SIZE_LG = 6;
+
+const PreviewContainer = () => {
+  const [large, setLarge] = useState(false);
+  const [play, setPlay] = useState(false);
+  const cellSize = large ? CELL_SIZE_LG : CELL_SIZE_SM;
+
+  const handleToggleSize = () => {
+    setLarge((prevLarge) => !prevLarge);
+  };
+  const handleTogglePlay = () => {
+    setPlay((prevPlay) => !prevPlay);
+  };
+
+  return (
+    <>
+      <PreviewHandler
+        play={play}
+        large={large}
+        onToggleSize={handleToggleSize}
+        onTogglePlay={handleTogglePlay}
+      />
+      <div className="col-span-5"></div>
+      <div className="bg-neutral-900 p-4">
+        <div className="flex h-full grow flex-col justify-between gap-1">
+          <Preview animate={play} cellSize={cellSize} />
+          <div className="relative py-2">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <ClockIcon className="h-3 w-3 text-gray-400" />
+            </div>
+            <input
+              type="number"
+              id="duration"
+              className="block w-full rounded-lg border border-gray-600 bg-input-color p-0.5 pl-3 text-center text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PreviewContainer;
--- frontend/src/components/PreviewHandler.tsx
+++ frontend/src/components/PreviewHandler.tsx
@@ -1,32 +1,66 @@
 import {
   ArrowsPointingInIcon,
+  ArrowsPointingOutIcon,
+  PauseIcon,
   PlayIcon,
   WindowIcon,
 } from "@heroicons/react/24/outline";
+import { useSearchParams } from "react-router-dom";
+import { useAppDispatch } from "../hooks/useRedux";
+import { openModal } from "../store";
 
-const PreviewHandler = () => {
+type PreviewHandlerProps = {
+  play: boolean;
+  large: boolean;
+  onToggleSize: () => void;
+  onTogglePlay: () => void;
+};
+
+const PreviewHandler = ({
+  play,
+  large,
+  onToggleSize,
+  onTogglePlay,
+}: PreviewHandlerProps) => {
+  const dispatch = useAppDispatch();
+  const [params, setParams] = useSearchParams();
+
   return (
     <div className="mb-4 mt-2 flex w-fit rounded bg-neutral-900">
       <div className="flex items-center justify-center p-2">
         <button
           type="button"
-          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-primary-color focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          onClick={onTogglePlay}
         >
-          <span className="sr-only">Pencil</span>
-          <PlayIcon className="h-6 w-6" aria-hidden="true" />
+          <span className="sr-only">Play</span>
+          {play ? (
+            <PauseIcon className="h-6 w-6" aria-hidden="true" />
+          ) : (
+            <PlayIcon className="h-6 w-6" aria-hidden="true" />
+          )}
         </button>
         <button
           type="button"
-          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-primary-color focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          onClick={onToggleSize}
         >
-          <span className="sr-only">Pencil</span>
-          <ArrowsPointingInIcon className="h-6 w-6" aria-hidden="true" />
+          <span className="sr-only">Size</span>
+          {large ? (
+            <ArrowsPointingInIcon className="h-6 w-6" aria-hidden="true" />
+          ) : (
+            <ArrowsPointingOutIcon className="h-6 w-6" aria-hidden="true" />
+          )}
         </button>
         <button
           type="button"
-          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          className="rounded-lg bg-neutral-900 p-1 text-gray-100 hover:text-primary-color focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          onClick={() => {
+            setParams({ ...params, modal: "true" });
+            dispatch(openModal("Preview"));
+          }}
         >
-          <span className="sr-only">Pencil</span>
+          <span className="sr-only">New window</span>
           <WindowIcon className="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
