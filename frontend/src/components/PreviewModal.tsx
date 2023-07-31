47614711b5f7c38d8e0868eec5b41cc411a2edb2
padosum <makalu171@gmail.com>
2023-07-31 14:37:00 +0900

    Feat: animate toggle 추가

--- frontend/src/components/PreviewModal.tsx
+++ frontend/src/components/PreviewModal.tsx
@@ -3,10 +3,19 @@ import { useAppDispatch } from "../hooks/useRedux";
 import { Modal } from "./common/Modal";
 import { closeModal } from "../store";
 import Preview from "./Preview";
+import { useState } from "react";
 
 const PreviewModal = () => {
-  const [params, setParams] = useSearchParams();
   const dispatch = useAppDispatch();
+
+  const [params, setParams] = useSearchParams();
+
+  const [animate, setAnimate] = useState(false);
+
+  const handleToggleAnimate = () => {
+    setAnimate((prevAnimate) => !prevAnimate);
+  };
+
   return (
     <Modal.Frame
       open={!!params.get("modal")}
@@ -19,7 +28,36 @@ const PreviewModal = () => {
     >
       <Modal.Head>Preview</Modal.Head>
       <Modal.Body>
-        <Preview animate={true} cellSize={10} />
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="view-type"
+              className="inline-flex cursor-pointer items-center rounded bg-neutral-900 p-1 text-gray-100"
+            >
+              <input
+                id="view-type"
+                type="checkbox"
+                role="switch"
+                className="peer hidden"
+                onChange={handleToggleAnimate}
+                checked={animate}
+              />
+              <span
+                aria-hidden="true"
+                className="rounded bg-primary-color px-4 py-2 text-sm text-black peer-checked:bg-neutral-900 peer-checked:text-gray-100"
+              >
+                Static
+              </span>
+              <span
+                aria-hidden="true"
+                className="rounded bg-neutral-900 px-4 py-2 text-sm peer-checked:bg-primary-color peer-checked:text-black"
+              >
+                Animation
+              </span>
+            </label>
+          </div>
+          <Preview animate={animate} cellSize={10} />
+        </div>
       </Modal.Body>
     </Modal.Frame>
   );
