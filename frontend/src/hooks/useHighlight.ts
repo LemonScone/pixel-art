a9c65724d75c4a6c9f931d6b40a40ff030619060
padosum <makalu171@gmail.com>
2023-08-03 14:35:44 +0900

    Fix: hljs.registerLanguage를 처음에 실행하도록 수정

--- frontend/src/hooks/useHighlight.ts
+++ frontend/src/hooks/useHighlight.ts
@@ -1,7 +1,6 @@
 import hljs from "highlight.js/lib/core";
 import css from "highlight.js/lib/languages/css";
 import "highlight.js/styles/atom-one-dark.css";
-import { useEffect } from "react";
 
 const languageMap = {
   css,
@@ -9,11 +8,11 @@ const languageMap = {
 
 type Language = typeof languageMap;
 
-export const useHighlight = (language: keyof Language) => {
-  useEffect(() => {
-    hljs.registerLanguage(language, languageMap[language]);
-  }, [language]);
+Object.keys(languageMap).forEach((lan) => {
+  hljs.registerLanguage(lan, languageMap[lan as keyof Language]);
+});
 
+export const useHighlight = (language: keyof Language) => {
   const renderHighlight = (content = "") => {
     const result = language
       ? hljs.highlight(language, content)
