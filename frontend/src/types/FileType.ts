dda649b20d5d06f91ee32dc829a4c9bce17b98ba
padosum <makalu171@gmail.com>
2023-08-10 16:59:06 +0900

    Feat: FileType 추가

--- frontend/src/types/FileType.ts
+++ frontend/src/types/FileType.ts
@@ -1,4 +1,4 @@
-const fileTypes = ["PNG", "GIF", "Sprite"] as const;
+const fileTypes = ["PNG", "GIF", "Sprite", "SVG"] as const;
 type FileType = (typeof fileTypes)[number];
 
 export type { FileType };
