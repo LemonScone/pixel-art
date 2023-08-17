412c6b311a23956eb38ef97626e24951945e55cb
padosum <makalu171@gmail.com>
2023-08-17 14:51:46 +0900

    Feat: INVALID USER constants 추가

--- frontend/src/tests/fixtures/auth.ts
+++ frontend/src/tests/fixtures/auth.ts
@@ -6,7 +6,11 @@ const VALID_USER = {
   current: 0,
 };
 
+const INVALID_USER = {
+  email: "invalid@invalid.com",
+};
+
 const VALID_TOKEN =
   "ruRZEEiCKcmK/6GPeUukcOV9aqnzP5c4dqMFX4D1HbHMBYU+oM3Y4ZTsrYBqh3G6mH0CEmzljQ8uI/9lon6ZD8KdFyOglXFqGKyMuSTZAMkqLVOmR4TmWCzowdAnvV/EGsg0qzXcaw8LhHBx5aD94O5uBMQQQQYvGEo8vvmn2T9rPUOCQ8a3Y+8cf6aDNUZ7uagSVdSkePyOUvw8Chb74S0078y3Aw6mBUPrhXe9rFjTuR71AwJOO34BIGChE47rBIdo1Qt9Q8Txx8j2UfgsMKMla+HCt4NHSQysh06x4WY/+zjubacCM5ye1y6rr7D0aoxFxsDl5kmQMNoYKjdufQ==";
 
-export { VALID_USER, VALID_TOKEN };
+export { VALID_USER, VALID_TOKEN, INVALID_USER };
