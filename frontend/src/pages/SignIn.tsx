ba3260528d3d7fb3ff02631b451d3d9d3dbcf7be
padosum <makalu171@gmail.com>
2023-08-17 14:53:20 +0900

    Fix: 비밀번호 찾기 페이지 링크 추가

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import SignInForm from "../components/SignIn/SignInForm";
 
 const SignIn = () => {
@@ -5,9 +7,12 @@ const SignIn = () => {
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="w-[32rem] rounded bg-neutral-900 p-10 shadow-md">
         <h2 className="text-center text-2xl text-gray-100">
-          Sign in to gridapixel
+          Sign in to Grida Pixel
         </h2>
         <SignInForm />
+        <div className="mt-2 flex justify-end text-sm text-gray-100">
+          <Link to="/signhelp">Need help?</Link>
+        </div>
       </div>
     </div>
   );
